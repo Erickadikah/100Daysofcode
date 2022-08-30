b3c34a4911930af96bfdb37bd2129b944247a243
ERICK ADIKAH <104826804+Erickadikah@users.noreply.github.com>
2022-08-30 12:28:49 +0300

    fix my code

---
+++ Day 1/main.py
@@ -0,0 +1,6 @@
+# fix my code
+
+print("Day 1 - string manipulation")
+print('string Concatination is done with the "+" sign')
+print('e.g. print("Hello " + "world")')
+print("New lines can be creared with a backslash and n.")
