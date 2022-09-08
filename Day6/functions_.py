cb276f9d150c2e587ac36783d151bf18afe605dc
ERICK ADIKAH <104826804+Erickadikah@users.noreply.github.com>
2022-09-08 10:25:27 +0300

    new

---
+++ Day6/functions_.py
@@ -0,0 +1,26 @@
+# code chalange robot. and code efficiency.
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# for step in range(6):
+#     jump()
+
+# indetation importance.
+# while loop
+number_of_hurdles = 6
+while number_of_hurdles > 0:
+    jump()
+    number_of_hurdles -= 1
+    print(number_of_hurdles)
